5b70be333a44b82a8f0fa1609267e806d3123880
Torben <59419684+entorb@users.noreply.github.com>
2025-05-15 21:48:41 +0200

    r00_Intro and minor changes

--- src/reports/r01_Text_and_Layout.py
+++ src/reports/r01_Text_and_Layout.py
@@ -12,14 +12,58 @@ st.title(__doc__[:-1])  # type: ignore
 st.header("Columns")
 st.subheader("3 same size columns")
 
-col1, col2, col3 = st.columns(3)
-col1.subheader("Col1")
-col2.subheader("Col2")
-col3.subheader("Col3")
+DUMMY_TEXT = """This is a long plain dummy text. This is a long plain dummy text. This is a long plain dummy text. This is a long plain dummy text."""
+
+cols = st.columns(3)
+cols[0].subheader("Col1")
+cols[0].write(DUMMY_TEXT)
+cols[1].subheader("Col2")
+cols[1].write(DUMMY_TEXT)
+cols[2].subheader("Col3")
+cols[2].write(DUMMY_TEXT)
 
 st.subheader("1/3 and 3/4 columns with only the first used")
-col1, _ = st.columns([1, 3])  # 1/3 and 3/4 columns
-col1.subheader("Col1")
+cols = st.columns([1, 3])  # 1/3 and 3/4 columns
+cols[0].subheader("Col1")
+cols[0].write(DUMMY_TEXT)
+
+
+st.header("Text")
+cols = st.columns(2)
+cols[0].subheader("Plain Text")
+cols[0].write(DUMMY_TEXT)
+
+
+cols[1].subheader("Markdown")
+cols[1].markdown("""
+This is a **markdown** text with *italics* and `code` and [link](https://streamlit.io).
+""")
+
+
+st.subheader("LaTeX")
+# 2 equals 1
+cols = st.columns(2)
+cols[0].latex(r"""
+\begin{align}
+a &= b\\
+a^2 &= ab\\
+2 a^2 &= a^2 + ab\\
+2 a^2 -2ab &= a^2 - ab\\
+2 a (a-b) &= a (a-b)\\
+2 a &= a \\
+2 &= 1 \\
+\end{align}
+""")
+
+# Maxwell equations
+cols[1].latex(r"""
+\begin{align}
+\nabla \cdot \vec{E} & = \frac{\rho}{\varepsilon_0} \\
+\nabla \cdot \vec{B} & = 0 \\
+\nabla \times \vec{E} & = -\frac{\partial \vec{B}}{\partial t} \\
+\nabla \times \vec{B} & = \mu_0 \vec{J} + \mu_0 \varepsilon_0 \frac{\partial \vec{E}}{\partial t}
+\end{align}
+""")  # noqa: E501
 
 
 # st.header("Dark Layout Toggle")
