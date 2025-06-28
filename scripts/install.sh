46f572bf73e2bd285b305af976e816fa0822f5d5
Torben <59419684+entorb@users.noreply.github.com>
2025-06-28 06:08:43 +0200

    minor scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,5 @@ cd $(dirname $0)/..
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
+pip install --upgrade -r requirements-dev.txt
 pip freeze >requirements-all.txt
