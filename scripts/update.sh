3655c108821c6f73321a6ce0678d74be6c4be1c8
Torben <59419684+entorb@users.noreply.github.com>
2026-04-12 16:35:34 +0200

    Tools

---
+++ scripts/update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+uv remove chardet msal pandas streamlit xlsxwriter
+uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+uv add chardet msal pandas streamlit xlsxwriter
+uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+
+uv lock --upgrade
+uv sync --upgrade
+
+# ruff
+uv run ruff format
+uv run ruff check --fix
+
+# pre-commit
+uv run pre-commit autoupdate
+uv run pre-commit run --all-files
+
+echo DONE
