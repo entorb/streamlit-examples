4df2737311f70ef6b04838f1f7353bcd0d464587
Torben <59419684+entorb@users.noreply.github.com>
2026-08-07 23:05:09 +0200

    Update test_run_all_pages.py

--- tests/test_run_all_pages.py
+++ tests/test_run_all_pages.py
@@ -10,8 +10,10 @@ from helper_mem_logger import MemLogger
 
 warnings.filterwarnings("ignore", message=".*streamlit.runtime.scriptrunner_utils.*")
 
-sys.path.insert(0, (Path(__file__).parent.parent / "src").as_posix())
-sys.path.insert(0, (Path(__file__).parent.parent / "src" / "reports").as_posix())
+SRC = Path(__file__).parent.parent / "src"
+
+sys.path.insert(0, SRC.as_posix())
+sys.path.insert(0, (SRC / "reports").as_posix())
 
 MEM_LOGGER = MemLogger(Path(__file__).parent / "mem_logger.tsv")
 
@@ -40,7 +42,7 @@ def init_and_run(path: Path) -> AppTest:
 # tests
 def test_all_pages() -> None:
     """Open all pages and check for errors and warnings."""
-    for p in sorted(Path("src/reports").glob("*.py")):
+    for p in sorted((SRC / "reports").glob("*.py")):
         f = p.stem
         if f.startswith("_"):
             continue
@@ -53,7 +55,7 @@ def test_all_pages() -> None:
 
 def test_single_page() -> None:
     """Open specific page and set input value."""
-    p = Path("src/reports/r02_inputs.py")
+    p = SRC / "reports" / "r02_inputs.py"
     f = p.stem
     t = f[4:]
     print(t)
